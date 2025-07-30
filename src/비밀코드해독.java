e38a3ee3ef6cec72e5a4d8d7e3c40e3f5cf7ced0
st120712 <zzw123@naver.com>
2025-07-31 00:58:33 +0900

    비밀코드해독 - backtracking

---
+++ src/비밀코드해독.java
@@ -0,0 +1,124 @@
+
+public class 비밀코드해독 {
+
+    static int answer = 0;
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[][] q = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {3, 7, 8, 9, 10}, {2, 5, 7, 9, 10}, {3, 4, 5, 6, 7}};
+        int[] ans = {2, 3, 4, 3, 3};
+
+        System.out.println(solution(n, q, ans));
+    }
+
+    public static int solution(int n, int[][] q, int[] ans) {
+        backTracking(0, 1, n, 5, new IArrayList(), q, ans);
+
+        System.out.println();
+        return answer;
+    }
+
+    public static void backTracking(int depth, int start, int n, int k, IArrayList current, int[][] q, int[] ans) {
+        if (depth == k) {
+            answer++;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+
+            boolean valid = true;
+            for (int j = 0; j < q.length; j++) {
+                int count = countOverlap(current.toArray(), q[j]);
+                int remainingCount = ans[j] - count;
+                int remainingChoice = k - current.size();
+
+                if (count > ans[j] || remainingCount > remainingChoice) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                backTracking(depth + 1, i + 1, n, k, current, q, ans);
+            }
+
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static int countOverlap(int[] arr, int[] q) {
+        int count = 0;
+        for (int i : arr) {
+            for (int j : q) {
+                if (i == j) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static class IArrayList {
+
+        private int[] values;
+        private int size;
+
+        public IArrayList() {
+            values = new int[10];
+            size = 0;
+        }
+
+        public void add(int n) {
+            if (size == values.length) {
+                grow();
+            }
+            values[size++] = n;
+        }
+
+        public void remove(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Invalid index: " + index);
+            }
+            for (int i = index; i < size - 1; i++) {
+                values[i] = values[i + 1];
+            }
+            values[size - 1] = 0;
+            size--;
+        }
+
+        public int get(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Invalid index: " + index);
+            }
+            return values[index];
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public int[] toArray() {
+            return java.util.Arrays.copyOf(values, size);
+        }
+
+        public void print() {
+
+            String str = "{ ";
+
+            for (int i = 0; i < size; i++) {
+                str += values[i] + " ";
+            }
+
+            str += "}";
+
+            System.out.println(str);
+        }
+
+        private void grow() {
+            int newCapacity = values.length * 2;
+            values = java.util.Arrays.copyOf(values, newCapacity);
+        }
+    }
+}
--- src/지게차와크레인.java
+++ src/지게차와크레인.java
@@ -1,7 +1,134 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 지게차와크레인 {
 
     public static void main(String[] args) {
+        String[] storage = {"HAH", "HBH", "HHH", "HAH", "HBH"};
 
+        String[] requests = {"C", "B", "B", "B", "B", "H"};
+
+        System.out.println(floodFill(storage, requests));
+    }
+
+    public static int floodFill(String[] storage, String[] requests) {
+        int answer = 0;
+
+        char[][] grid = convertToGrid(storage);
+
+        for (String request : requests) {
+            char[][] newGrid = {};
+
+            if (request.length() == 1) {
+                newGrid = forklift(grid, request.charAt(0));
+            }
+
+            if (request.length() == 2 && request.charAt(0) == request.charAt(1)) {
+                newGrid = crane(grid, request.charAt(0));
+            }
+
+            grid = newGrid;
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] != '0') {
+                    answer += 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static char[][] convertToGrid(String[] storage) {
+        int row = storage.length + 2;
+        int column = storage[0].length() + 2;
+        char[][] grid = new char[row][column];
+
+        for (int i = 0; i < column; i++) {
+            grid[0][i] = '0';
+            grid[row - 1][i] = '0';
+        }
+
+        for (int i = 1; i < row - 1; i++) {
+            grid[i] = ('0' + storage[i - 1] + '0').toCharArray();
+        }
+
+        return grid;
+    }
+
+    public static char[][] forklift(char[][] grid, char target) {
+        int[] dx = new int[]{1, -1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
+
+        int row = grid.length;
+        int column = grid[0].length;
+
+        boolean[][] accessible = new boolean[row][column];
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if ((i == 0 || i == row - 1 || j == 0 || j == column - 1) && grid[i][j] == '0') {
+                    accessible[i][j] = true;
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] pos = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = pos[0] + dx[i];
+                int ny = pos[1] + dy[i];
+
+                if (nx < 0 || nx > row - 1 || ny < 0 || ny > column - 1) {
+                    continue;
+                }
+
+                if (grid[nx][ny] == '0' && !accessible[nx][ny]) {
+                    accessible[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        for (int i = 1; i < row - 1; i++) {
+            for (int j = 1; j < column - 1; j++) {
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx < 0 || nx > row - 1 || ny < 0 || ny > column - 1) {
+                        continue;
+                    }
+
+                    if (grid[i][j] == target && accessible[nx][ny]) {
+                        grid[i][j] = '0';
+                        break;
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    public static char[][] crane(char[][] grid, char target) {
+        int row = grid.length;
+        int column = grid[0].length;
+
+        for (int i = 1; i < row - 1; i++) {
+            for (int j = 1; j < column - 1; j++) {
+                if (grid[i][j] == target) {
+                    grid[i][j] = '0';
+                }
+            }
+        }
+
+        return grid;
     }
 }
