23a6a93ec66b8c72f7476e3b84e665e8f756b30f
st120712 <zzw123@naver.com>
2025-08-21 13:29:59 +0900

    배달 - 한 번 더 풀어보기(반복학습)

--- src/level2/배달.java
+++ src/level2/배달.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class 배달 {
 
@@ -18,42 +17,47 @@ public class 배달 {
     }
 
     public static int solution(int N, int[][] road, int K) {
-        List<int[]>[] list = new ArrayList[N + 1];
-
+        // 양방향 인접 리스트 생성
+        List<int[]>[] g = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
+            g[i] = new ArrayList<>();
         }
-
+        // 최단거리를 구하는 문제이므로 각 노드에서 걸리는 시간도 같이 넣어줌 : 다익스트라 알고리즘
         for (int[] r : road) {
-            int duration = r[2];
             int u = r[0];
             int v = r[1];
+            int w = r[2];
 
-            list[u].add(new int[]{v, duration});
-            list[v].add(new int[]{u, duration});
+            g[u].add(new int[]{v, w});
+            g[v].add(new int[]{u, w});
         }
 
+        // 첫 노드에서 해당 번째 노드까지 걸리는 최소 시간을 저장해놓는 배열
         int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(dist, -1);
+        // 이 문제는 1번 노드부터 시작하므로 1번 노드의 값은 0
         dist[1] = 0;
 
-        Queue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        q.offer(new int[]{0, 1});
+        // {d, u} 걸린 시간과 현재 노드를 저장
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        // 시작 노드를 넣음
+        pq.offer(new int[]{0, 1});
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
             int d = cur[0];
             int u = cur[1];
+            // 걸린 시간이 이전에 걸린 시간 보다 크면 스킵
             if (d > dist[u]) {
                 continue;
             }
-            for (int[] nx : list[u]) {
-                int v = nx[0];
-                int w = nx[1];
-                int nd = d + w;
-                if (nd < dist[v]) {
-                    dist[v] = nd;
-                    q.offer(new int[]{nd, v});
+            for (int[] l : g[u]) {
+                int w = l[1];
+                int v = l[0];
+
+                if (dist[v] == -1 || dist[v] > d + w) {
+                    dist[v] = d + w;
+                    pq.offer(new int[]{dist[v], v});
                 }
             }
         }
