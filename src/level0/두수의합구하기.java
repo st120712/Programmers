8a7edba9a0b275f17195f9f46d9676f829d2e590
st120712 <zzw123@naver.com>
2025-08-21 13:06:38 +0900

    두 수의 합 구하기

---
+++ src/level0/두수의합구하기.java
@@ -0,0 +1,15 @@
+package level0;
+
+public class 두수의합구하기 {
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
