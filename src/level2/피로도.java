e91a1f243a80a0b895a2ff120c3b8fe667996012
dongho <zzw123@naver.com>
2025-08-23 17:19:27 +0900

    미완

---
+++ src/level2/피로도.java
@@ -0,0 +1,33 @@
+package level2;
+
+public class 피로도 {
+  public static void main(String[] args) {
+    int[][] dungeons = { { 80, 20 }, { 50, 40 }, { 30, 10 } };
+    int k = 80;
+
+    System.out.println(solution(k, dungeons));
+  }
+
+  public static int solution(int k, int[][] dungeons) {
+
+    int cnt = 0;
+
+    boolean[] visited = new boolean[dungeons.length];
+    for (int i = 0; i < dungeons.length; i++) {
+      visited[i] = true;
+      cnt = Math.max(cnt, dfs(k, dungeons[i], 0, dungeons));
+    }
+    return 0;
+  }
+
+  public static int dfs(int k, int[] dungeon, int cnt, int[][] dungeons) {
+    if (k < dungeon[0]) {
+      return cnt;
+    }
+
+    cnt++;
+    for (int[] d : dungeons) {
+      return dfs(k - dungeon[1], d, cnt, dungeons);
+    }
+  }
+}
