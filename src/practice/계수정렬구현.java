e1d944f0efc39dc0c0585a7adaf71968f3df182f
st120712 <zzw123@naver.com>
2025-09-12 11:24:32 +0900

    .

--- src/practice/계수정렬구현.java
+++ src/practice/계수정렬구현.java
@@ -1,35 +1,20 @@
 package practice;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class 계수정렬구현 {
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5};
-        int[] arr2 = {2, 4, 6};
-        System.out.println(Arrays.toString(solution(arr1, arr2)));
+        long n = 118372;
+        System.out.println(solution(n));
     }
 
-    public static int[] solution(int[] arr1, int[] arr2) {
-        int p1 = 0;
-        int p2 = 0;
-
-        int[] ans = new int[arr1.length + arr2.length];
-
-        for (int i = 0; i < ans.length; i++) {
-            if (p1 == arr1.length) {
-                ans[i] = arr2[p2++];
-            } else if (p2 == arr1.length) {
-                ans[i] = arr1[p1++];
-            } else {
-                if (arr1[p1] < arr2[p2]) {
-                    ans[i] = arr1[p1++];
-                } else {
-                    ans[i] = arr2[p2++];
-                }
-            }
-        }
-
-        return ans;
+    public static long solution(long n) {
+        String[] digit = ("" + n).split("");
+        Arrays.sort(digit, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(digit).forEach(sb::append);
+        return Long.parseLong(sb.toString());
     }
 }
--- src/practice/연습장.java
+++ src/practice/연습장.java
@@ -5,99 +5,22 @@ import java.util.Arrays;
 public class 연습장 {
 
     public static void main(String[] args) {
-        int[][] board = {
-            {5, 3, 0, 0, 7, 0, 0, 0, 0},
-            {6, 0, 0, 1, 9, 5, 0, 0, 0},
-            {0, 9, 8, 0, 0, 0, 0, 6, 0},
-            {8, 0, 0, 0, 6, 0, 0, 0, 3},
-            {4, 0, 0, 8, 0, 3, 0, 0, 1},
-            {7, 0, 0, 0, 2, 0, 0, 0, 6},
-            {0, 6, 0, 0, 0, 0, 2, 8, 0},
-            {0, 0, 0, 4, 1, 9, 0, 0, 5},
-            {0, 0, 0, 0, 8, 0, 0, 7, 9}
-        };
-        solution(board);
-        for (int[] b : board) {
-            System.out.println(Arrays.toString(b));
-        };
+        int[] numbers = {6, 10, 2};
+        System.out.println(solution(numbers));
     }
 
-    static int[][] Board;
+    public static String solution(int[] numbers) {
+        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
 
-    public static int[][] solution(int[][] board) {
-        Board = board;
-        findSolution();
-        return board;
-    }
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
 
-    static boolean findSolution() {
-        Block emptyPos = findEmptyPosition();
-
-        if (emptyPos == null) {
-            return true;
+        if (arr[0].equals("0")) {
+            return "0";
         }
 
-        int row = emptyPos.i;
-        int col = emptyPos.j;
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(arr).forEach(sb::append);
 
-        for (int num = 1; num <= 9; num++) {
-            if (isValid(num, row, col)) {
-                Board[row][col] = num;
-                if (findSolution()) {
-                    return true;
-                }
-                Board[row][col] = 0;
-            }
-        }
-        return false;
-    }
-
-    static Block findEmptyPosition() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (Board[i][j] == 0) {
-                    return new Block(i, j);
-                }
-            }
-        }
-        return null;
-    }
-
-    static boolean isValid(int num, int row, int col) {
-        return !(inRow(num, row) || inCol(num, col) || inBox(num, row, col));
-    }
-
-    static boolean inRow(int num, int row) {
-        return Arrays.stream(Board[row]).anyMatch(n -> n == num);
-    }
-
-    static boolean inCol(int num, int col) {
-        for (int i = 0; i < 9; i++) {
-            if (Board[i][col] == num) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static boolean inBox(int num, int row, int col) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (Board[(row / 3) * 3 + i][(col / 3) * 3 + j] == num) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    static class Block {
-
-        int i, j;
-
-        public Block(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
+        return sb.toString();
     }
 }
