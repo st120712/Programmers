fffdd6b7d9bf8b8300371c8cc9b10e583acb5f26
dongho <zzw123@naver.com>
2025-09-19 01:24:14 +0900

    2 x n 타일링

---
+++ src/level2/l2xn타일링.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class l2xn타일링 {
+  public static void main(String[] args) {
+
+  }
+
+  public static int solution(int n) {
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    return dp[n];
+  }
+}
