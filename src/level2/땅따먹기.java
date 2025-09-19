40e1c72939538a11aa668e2c4a6958be5cbe59cc
st120712 <zzw123@naver.com>
2025-09-19 16:43:08 +0900

    땅따먹기2

--- src/level2/땅따먹기.java
+++ src/level2/땅따먹기.java
@@ -1,39 +1,33 @@
 package level2;
 
+import java.util.Arrays;
+
 public class 땅따먹기 {
-  public static void main(String[] args) {
-    int[][] land = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
 
-    System.out.println(solution(land));
-  }
+    public static void main(String[] args) {
+        int[][] land = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
 
-  public static int solution(int[][] land) {
-    int r = land.length;
-    int c = land[0].length;
-    int[][] dp = new int[r][c];
-
-    for (int i = 0; i < c; i++) {
-      dp[0][i] = land[0][i];
+        System.out.println(solution(land));
     }
 
-    for (int i = 1; i < r; i++) {
-      for (int j = 0; j < c; j++) {
-        for (int k = 0; k < c; k++) {
-          if (k != j) {
-            dp[i][j] = Math.max(dp[i][j], dp[i - 1][k] + land[i][j]);
-          }
+    public static int solution(int[][] land) {
+        int r = land.length;
+        int c = land[0].length;
+
+        int[][] dp = new int[r][c];
+        dp[0] = Arrays.copyOf(land[0], c);
+
+        for (int i = 1; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                for (int k = 0; k < c; k++) {
+                    if (j == k) {
+                        continue;
+                    }
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][k] + land[i][j]);
+                }
+            }
         }
-      }
+
+        return Arrays.stream(dp[r - 1]).max().getAsInt();
     }
-
-    int ans = 0;
-
-    for (int n : dp[r - 1]) {
-      if (ans < n) {
-        ans = n;
-      }
-    }
-
-    return ans;
-  }
 }
